2ed27d091c25bbee038e27dbc75a0207dd471375
Haci11 <46707887+Haci11@users.noreply.github.com>
2023-12-20 23:07:45 +0100

    added necessary npms

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import inject from "@stylexjs/dev-runtime";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+inject({
+  classNamePrefix: "x",
+  dev: true,
+  test: false,
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
